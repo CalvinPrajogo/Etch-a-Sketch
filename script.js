d5bdf788a85caa29b4668ec947d5b468c16e490f
Calvin Prajogo <calprajogo@gmail.com>
2025-05-22 12:23:43 -0500

    Create functions to make square div and generate grid

---
+++ script.js
@@ -0,0 +1,19 @@
+container = document.querySelector("#container")
+
+function createSquareDiv(size) {
+    const div = document.createElement("div")
+    div.classList.add("square-div")
+    div.style.width = `${size}px`
+    div.style.height = `${size}px`
+    div.style.border = "solid black"
+    return div
+}
+
+function createGrid(gridsize, squareSize) {
+    for (let i = 0; i < gridsize * gridsize; i++) {
+        const square = createSquareDiv(squareSize)
+        container.appendChild(square)
+    }
+}
+
+createGrid(16, 30)
